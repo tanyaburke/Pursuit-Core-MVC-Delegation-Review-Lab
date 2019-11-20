a2ea0ab5abc436452f91771d7afd30e0e09a59e8
tanyaburke <tanyaburke@pursuit.org>
2019-11-20 16:26:51 -0500

    Set up main story board.

---
+++ MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/SettingsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SettingsViewController.swift
+//  MVC+Delegation-Review-Lab
+//
+//  Created by Tanya Burke on 11/20/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    @IBOutlet weak var sliderSize: UISlider!
+    
+    @IBOutlet weak var stepperControl: UIStepper!
+    
+    @IBOutlet weak var sizeNumberLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+    }
+    
+  
+
+}
---
+++ MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/TableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TableViewCell.swift
+//  MVC+Delegation-Review-Lab
+//
+//  Created by Tanya Burke on 11/20/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+   
+}
